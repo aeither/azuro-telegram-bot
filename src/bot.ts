e85d86be54cf0ac5894a5e36f1077a1a486e7720
aeither <36173828+aeither@users.noreply.github.com>
2023-05-19 08:33:47 +0200

    gnosis gas tracker

--- src/bot.ts
+++ src/bot.ts
@@ -4,11 +4,23 @@ import {
 } from 'https://esm.sh/@azuro-org/dictionaries@3.0.1'
 import dayjs from 'https://esm.sh/dayjs@1.11.7'
 
-import { TELEGRAM_BOT_TOKEN } from '../lib/constants.ts'
+import { GATEWAY_FM_KEY, TELEGRAM_BOT_TOKEN } from '../lib/constants.ts'
 import { getBetsHistory } from '../lib/getBetsHistory.ts'
-import { Bot } from './deps.ts'
+import { Bot, Context, session, SessionFlavor } from './deps.ts'
+import { freeStorage } from 'https://deno.land/x/grammy_storages@v2.2.0/free/src/mod.ts'
 
-export const bot = new Bot(TELEGRAM_BOT_TOKEN, {
+interface RpcResponse {
+  jsonrpc: string
+  result: string
+  id: number
+}
+
+interface SessionData {
+  count: number
+}
+type BotContext = Context & SessionFlavor<SessionData>
+
+export const bot = new Bot<BotContext>(TELEGRAM_BOT_TOKEN, {
   client: {
     timeoutSeconds: 60,
     canUseWebhookReply: (method) => {
@@ -17,6 +29,13 @@ export const bot = new Bot(TELEGRAM_BOT_TOKEN, {
     },
   },
 })
+
+bot.use(
+  session({
+    initial: () => ({ count: 0 }),
+    storage: freeStorage<SessionData>(bot.token),
+  }),
+)
 
 /*
  * Bot Commands
@@ -62,6 +81,39 @@ bot.command('bets', async (ctx) => {
   } catch (error) {}
 
   ctx.reply(replyMessage, { parse_mode: 'Markdown' })
+})
+
+bot.command('gasprice', async (ctx) => {
+  const url = 'https://rpc.eu-central-2.gateway.fm/v4/gnosis/non-archival/mainnet'
+
+  const myHeaders = new Headers()
+  myHeaders.append('Content-Type', 'application/json')
+  myHeaders.append('Authorization', `Bearer ${GATEWAY_FM_KEY}`)
+
+  const raw = JSON.stringify({
+    method: 'eth_gasPrice',
+    params: [],
+    id: 1,
+    jsonrpc: '2.0',
+  })
+
+  const requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+  }
+
+  let gasPrice = null
+  try {
+    const response = await fetch(url, requestOptions)
+    const data: RpcResponse = await response.json()
+    console.log(data)
+    gasPrice = parseInt(data.result, 16)
+  } catch (error) {
+    console.log('error', error)
+  }
+
+  ctx.reply(`Gas Price: ${gasPrice}`)
 })
 
 bot.command('ping', (ctx) => ctx.reply(`Pong! ${new Date()} ${Date.now()}`))
